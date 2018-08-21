f264f5f6d97b317079b7f0dba591b359866126f2
Vahan Asryan <asryanvahandev@gmail.com>
2018-08-21 12:24:44 +0400

    add eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -11,4 +11,14 @@ module.exports = {
       },
     },
   ],
+  rules: {
+    'max-lines-per-function': 'off',
+    'import/no-cycle': 'off',
+    'import/no-self-import': 'off',
+    'import/no-useless-path-segments': 'off',
+    'react/jsx-child-element-spacing': 'off',
+    'react/no-access-state-in-setstate': 'off',
+    'react/no-this-in-sfc': 'off',
+    'react/no-unsafe': 'off',
+  },
 }
