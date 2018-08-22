0913569bb41960244ca5293f6bf22c2067d120ed
Vahan Asryan <asryanvahandev@gmail.com>
2018-08-22 16:32:43 +0400

    login integration test

--- client/src/__tests__/app.login.todo.js
+++ client/src/__tests__/app.login.todo.js
@@ -1,42 +1,80 @@
 // add a beforeEach for cleaning up state and intitializing the API
+import React from 'react'
+import axiosMock from 'axios'
+import {renderWithRouter, fireEvent, generate} from 'til-client-test-utils'
+
+import App from '../app'
+import {init as initAPI} from '../utils/api'
+
+beforeEach(() => {
+  window.localStorage.removeItem('token')
+  axiosMock.__mock.reset()
+  initAPI()
+})
 
 test('login as an existing user', async () => {
   // 🐨 render the app with the router provider and custom history
-  // 💰 const utils = renderWithRouter(<App />)
-  //
+  const utils = renderWithRouter(<App />)
+
   // 🐨 wait for the app to finish loading the mocked requests
-  // 💰 await utils.finishLoading()
-  //
+  await utils.finishLoading()
+
   // 🐨 navigate to login by clicking login-link
   // 💰 the link has text that matches /login/i
   // 💰 when you fireEvent.click on the login link, react-router will ignore
   // the click unless it's a "left click" which is based on the `button`
   // property. So as a second argument to `fireEvent.click`, pass `{button: 0}`
-  //
+  const loginButton = utils.getByText(/login/i)
+  const leftClick = {button: 0}
+
+  fireEvent.click(loginButton, leftClick)
+
   // 🐨 assert that window.location.href contains 'login'
-  //
+  expect(window.location.href).toContain('login')
+
   // 🐨 fill out the form
   // 💰 generate.loginForm()
   // 💰 get the username and password fields and set their values
-  //
+  const fakeUser = generate.loginForm()
+  const form = utils.container.querySelector('form')
+  const usernameNode = utils.getByLabelText('Username')
+  const passwordNode = utils.getByLabelText('Password')
+  usernameNode.value = fakeUser.username
+  passwordNode.value = fakeUser.password
+
   // Now we need to prepare our axios mock to handle the form submission properly:
   // use the axiosMock.__mock.instance
   // to mock out the post implementation
   // it should return the fake user + a token
   // which you can generate with generate.token(fakeUser)
   // 💰 you may want to look at the final version for this one...
-  //
+  const {post} = axiosMock.__mock.instance
+  const token = generate.token(fakeUser)
+  post.mockImplementationOnce(() =>
+    Promise.resolve({
+      data: {user: {...fakeUser, token}},
+    }),
+  )
+
   // 🐨 submit form by clicking on the submit button
+  fireEvent.submit(form)
   //
   // 🐨 wait for the mocked requests to finish
-  // 💰 await utils.finishLoading()
-  //
+  await utils.finishLoading()
+
   // 🐨 now make some assertions:
   // assert post was called correctly
   // assert localStorage is correct
   // assert the user was redirected (window.location.href)
   // assert the username display is the fake user's username
   // assert the logout button exists
+  expect(post).toHaveBeenCalled()
+  expect(window.localStorage.getItem('token')).toBe(token)
+  expect(window.location.href).not.toContain('login')
+  expect(utils.getByText(fakeUser.username).textContent).toEqual(
+    fakeUser.username,
+  )
+  expect(utils.getByText(/logout/i)).toBeTruthy()
 })
 
 //////// Elaboration & Feedback /////////
